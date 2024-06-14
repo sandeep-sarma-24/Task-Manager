506add7d108663edae5c713e0cb515cb20f3c6bc
Sandeep <wayne241101@gmail.com>
2024-06-14 17:59:48 +0530

    added slight ui changes and made some improvements

--- backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routwe = express.Router();
-const { getProfile } = require('../controllers/profileControllers');
-const { verifyAccessToken } = require('../middlewares/authorization');
-const router = require('./authRoutes');
-
-router.get('/profile', verifyAccessToken, getProfile);
-
-module.exports = router;
